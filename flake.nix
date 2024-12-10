6399d9b74d027ac7cf678851fe13cf69eedb076b
Ben Mayer <info@leocodes.net>
2024-12-10 10:58:04 +0100

    Use nix to build package and switch to fenix for toolchain

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix.url = "github:nix-community/fenix";
+    naersk.url = "github:nix-community/naersk";
   };
 
   outputs =
@@ -11,6 +13,8 @@
       self,
       flake-utils,
       nixpkgs,
+      fenix,
+      naersk,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -18,13 +22,39 @@
         pkgs = (import nixpkgs) {
           inherit system;
         };
-      in
-      {
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            rustc
-            cargo
+
+        toolchain =
+          with fenix.packages.${system};
+          combine [
+            minimal.rustc
+            minimal.cargo
+            targets.x86_64-pc-windows-gnu.latest.rust-std
           ];
+
+        naersk' = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [ toolchain ];
+        };
+
+        defaultPackage = packages.print-guid;
+
+        packages.print-guid = naersk'.buildPackage {
+          src = ./.;
+          strictDeps = true;
+
+          depsBuildBuild = with pkgs; [
+            pkgsCross.mingwW64.stdenv.cc
+            pkgsCross.mingwW64.windows.pthreads
+          ];
+
+          # Tells Cargo that we're building for Windows.
+          # (https://doc.rust-lang.org/cargo/reference/config.html#buildtarget)
+          CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
         };
       }
     );
